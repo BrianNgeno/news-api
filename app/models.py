8d02930e09cda75f1ae52c181fa3b97ab441cce3
brianngeno <bkn.ngeno@gmail.com>
2018-08-31 17:48:00 +0300

    added class headlines

---
+++ app/models.py
@@ -0,0 +1,12 @@
+class Healines:
+    '''
+    class that defines the movie object
+    '''
+    
+    def __init__(self,id,name,aurthor,description,url,urlToImage,publishedAt):
+        self.id = id
+        self.name = name
+        self.aurthor = aurthor
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
--- config.py
+++ config.py
@@ -2,22 +2,24 @@ import os
 '''
 this helps get the api base url and key from the remote
 '''
-def Config():
+class Config():
     '''
     '''
-    NEWS_API_BASE_URL = 'https://newsapi.org/v2/{}?api_key={}'
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/{}?apiKey={}'
     NEWS_API_KEY = os.environment.get('NEWS_API_KEY')
     SECRET_KEY = os.environment.get('SECRET_KEY')
-    @staticmethod
-    def init_app(app):
-        pass
 
-def ProdConfig(Config):
+@staticmethod
+def init_app(app):
     pass
 
-def DevConfig(Config):
+class ProdConfig(Config):
+    pass
+
+class DevConfig(Config):
     DEBUG True
-    config_option ={
-        'production'= ProdConfig,
-        'development' = DevConfig
-    }
+
+config_option ={
+    'production'= ProdConfig,
+    'development' = DevConfig
+}
