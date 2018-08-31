a14545bc59c2d7b9fe06e5e58ccb31ee2425b683
brianngeno <bkn.ngeno@gmail.com>
2018-08-31 12:51:45 +0300

    added the content block tags to index.html and the style tags

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
 import os
+'''
+this helps get the api base url and key from the remote
+'''
 def Config():
+    '''
+    '''
     NEWS_API_BASE_URL = 'https://newsapi.org/v2/{}?api_key={}'
     NEWS_API_KEY = os.environment.get('NEWS_API_KEY')
     SECRET_KEY = os.environment.get('SECRET_KEY')
--- start.sh
+++ start.sh
@@ -1,2 +1,5 @@
 NEWS_API_KEY='efec2b3eaf424382ac83bf7684ef6491'
 NEWS_SECRET_KEY='lost'
+'''
+providing the apikey and secretkey to the app
+'''
