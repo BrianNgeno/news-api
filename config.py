65de190781aa5ea07d422eac4e45459d1e70104c
brianngeno <bkn.ngeno@gmail.com>
2018-08-31 12:29:18 +0300

    added the api key and secret key to start.sh

---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+def Config():
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/{}?api_key={}'
+    NEWS_API_KEY = os.environment.get('NEWS_API_KEY')
+    SECRET_KEY = os.environment.get('SECRET_KEY')
+    @staticmethod
+    def init_app(app):
+        pass
+
+def ProdConfig(Config):
+    pass
+
+def DevConfig(Config):
+    DEBUG True
+    config_option ={
+        'production'= ProdConfig,
+        'development' = DevConfig
+    }
