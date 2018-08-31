88da275e522076cb83bbdd0350798b5e15a136df
brianngeno <bkn.ngeno@gmail.com>
2018-08-31 15:59:36 +0300

    added a requests

---
+++ app/request.py
@@ -0,0 +1,13 @@
+import urllib.request,json
+from .models import News
+
+#getting the api_key
+api_key = None
+
+#getting the news base_url
+base_url = None
+
+def configure_request(app):
+    global api_key,base_url
+    api_key = app.config['NEWS_API_KEY']
+    base_url = app.config['NEWS_API_BASE_URL']
